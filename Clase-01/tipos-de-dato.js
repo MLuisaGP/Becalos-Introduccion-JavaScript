1b0fd97d8ca8a2c61f8ba6745943fe54f8284807
LuisaGa96 <luisagalazmp@gmail.com>
2025-02-04 19:14:16 -0700

    Archivo de tipo de datos

---
+++ Clase-01/tipos-de-dato.js
@@ -0,0 +1,12 @@
+console.log(typeof 42);//number
+console.log(typeof 'Veinticinco');//string
+console.log(typeof -666);//number
+console.log(typeof true);//bool
+console.log(typeof 0);//number
+console.log(typeof '');//string
+console.log(typeof null);//object  |  null
+console.log(typeof undefined);//undefined
+console.log(typeof FALSE);// undefined tiene que estar en minusculas para ser bool
+console.log(typeof (8>5));// boolean
+console.log(typeof 8>5);// false
+
