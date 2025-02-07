c22f6f7751c89214861f9850d8834b17fe8855f2
LuisaGa96 <luisagalazmp@gmail.com>
2025-02-06 20:08:55 -0700

    Se mejoro la actividad extra realizada en clase

--- Clase-02/activity-ext.js
+++ Clase-02/activity-ext.js
@@ -1,38 +1,35 @@
 //Numeros pares e impares
 let numeroAleatorio = Math.floor(Math.random()*101); 
 
-if(numeroAleatorio%2 === 0){
-    console.log("El numero",numeroAleatorio,"es par");
-}else{
-    console.log("El numero",numeroAleatorio,"es impar");
+let par = numeroAleatorio%2 === 0 ? "par":"inpar";
+
+//Numero primo
+let isPrimo=true;
+for (let i = 2; i < numeroAleatorio; i++) {
+    if (numeroAleatorio % i === 0){
+        isPrimo=false;
+        break;
+    }
 }
+//Mostar que tipo de valor es el numero aleatorio
+console.log("El numero",numeroAleatorio,"es",par,"y es",(isPrimo?"primo":"compuesto"));
 
 
 // Meses
 
 let date = new Date();
 let month = date.getMonth() +1;
+let estacion;
 
 if(month>=3 && month<=5){
-    console.log('Es primavera');
+    estacion = "primavera.";
 }else if(month>=6 && month<=8){
-    console.log('Es verano');
+    estacion = "verano.";
 }else if(month>=9 && month<=11){
-    console.log('Es otoño');
+    estacion = "otoño.";
 }else if(month===1 || month===2 || month===12){
-    console.log('Es invierno');
+    estacion = "invierno.";
 }else{
-    console.log('Mes no valido');
+    estacion = "ninguna estacion. Mes no válido.";
 }
-
-//Numero primo
-
-function esPrimo(num) {
-    if (num < 2) return false;
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) return false;
-    }
-    return true;
-}
-const numero = Math.floor(Math.random() * 101);
-console.log(`${numero} es ${esPrimo(numero) ? "primo" : "compuesto"}`);
+console.log(`Estamos en ${estacion}`);
