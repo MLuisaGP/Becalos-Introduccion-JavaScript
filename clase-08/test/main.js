5c9c45c5cf34c884173c7e0f0c7ae0251f56ea31
LuisaGa96 <luisagalazmp@gmail.com>
2025-02-28 18:15:39 -0700

    Se realizaron notas de la clase final

---
+++ clase-08/test/main.js
@@ -0,0 +1,13 @@
+function moverImg(){
+    let test = document.getElementById('imagen');
+    let contenido = document.getElementById('cont');
+    let txt1 = document.getElementById('txt-1');
+    let txt2 = document.getElementById('txt-2');
+    test.classList.toggle('right');
+    test.classList.toggle('left');
+    contenido.classList.toggle('right');
+    contenido.classList.toggle('left');
+    txt1.classList.toggle('hidden');
+    txt2.classList.toggle('hidden');
+    t
+}
