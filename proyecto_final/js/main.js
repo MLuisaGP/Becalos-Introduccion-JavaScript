a9aec5af9dd989dbede91f2aaf88ac14450284ca
LuisaGa96 <luisagalazmp@gmail.com>
2025-03-01 17:35:04 -0700

    Se toma el proyecto de la clase 6 para referencia

---
+++ proyecto_final/js/main.js
@@ -0,0 +1,93 @@
+let botonSend = document.getElementById("btn-send");
+let inputComt = document.getElementById("comentario-text");
+let showComt = document.getElementById("cmts-show");
+let contComment = document.getElementById("contador-comentarios");
+let contLikes   = document.getElementById("contador-likes");
+let contShared  = document.getElementById("contador-shared");
+let btnLiked  = document.getElementById("btn-liked");
+let btnComment  = document.getElementById("btn-comment");
+let btnShared  = document.getElementById("btn-shared");
+let textoInicio = document.getElementById("sin-coment");
+
+let contadorComentario=0;
+let contadorLiked = 4;
+let conadorShared = 3;
+
+botonSend.addEventListener("click",function(event){
+    event.preventDefault();
+
+    if(contadorComentario==0){
+        showComt.removeChild(textoInicio);
+    }
+
+    if(inputComt.value=='')return;
+    let contenedor = document.createElement('div')
+    contenedor.className='cmt-elemento';
+
+    let nombre = document.createElement('b');
+    nombre.textContent='Lorena Días';
+    let fecha = document.createElement('span');
+    fecha.textContent=dateNow();
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML += '<i class="fa-solid fa-trash"></i>';
+    let comentario = document.createElement('p');
+    comentario.textContent=inputComt.value;
+
+    contenedor.appendChild(nombre);
+    contenedor.appendChild(fecha);
+    contenedor.appendChild(deleteBtn);
+    contenedor.appendChild(comentario);
+    showComt.insertBefore(contenedor,showComt.firstChild); //Agrega el comentario primero que todos los otros hijos
+    inputComt.value='';
+    contadorComentario++;
+    deleteBtn.addEventListener('click',function (){
+            let parentElement = this.parentElement;
+            parentElement.remove();
+            contadorComentario--;
+            contComment.textContent=contadorComentario;
+            if(contadorComentario==0){
+                showComt.innerHTML='<p id="sin-coment">Se el primero en comentar</p>';
+                textoInicio = document.getElementById("sin-coment");
+            }
+        }
+    );
+    contComment.textContent=contadorComentario;
+})
+
+let flagLiked = false;
+btnLiked.addEventListener('click',function(){
+    if(flagLiked){
+        contadorLiked--;
+        btnLiked.classList.remove("activo-btn");
+        flagLiked = !flagLiked ;
+    }else{
+        flagLiked = !flagLiked ;
+        contadorLiked++;
+        btnLiked.className="activo-btn";
+    }
+    contLikes.textContent=contadorLiked;
+})
+
+btnShared.addEventListener('click',function(){
+    alert('Haz compartido la imagen');
+    conadorShared++;
+    contShared.textContent=conadorShared;
+})
+
+btnComment.addEventListener('click',function(){
+    inputComt.focus();
+})
+
+
+function dateNow(){
+    const fechaActual = new Date();
+
+    const dia = String(fechaActual.getDate()).padStart(2, '0');
+    const mes = String(fechaActual.getMonth() + 1).padStart(2, '0'); // Los meses comienzan desde 0
+    const anio = String(fechaActual.getFullYear()); // Obtener los últimos dos dígitos
+    const hora = String(fechaActual.getHours()).padStart(2,'0'); // Obtener los últimos dos dígitos
+    const minuto = String(fechaActual.getMinutes()).padStart(2,'0'); // Obtener los últimos dos dígitos
+
+    const fechaFormateada = `${dia}-${mes}-${anio} ${hora}:${minuto}`;
+    return fechaFormateada;
+}
